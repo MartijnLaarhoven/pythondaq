cbe0ed9d7be8d7d7d606634f74d350f377a241ae
MartijnLaarhoven <martijnlaarhoven2001@gmail.com>
2022-11-11 16:45:31 +0100

    Update view.py

--- view.py
+++ view.py
@@ -32,3 +32,4 @@ with open("adruinodata.txt", "w",newline="") as f:
 # opdracht 3.7 is niet gelukt door de tijd.
 # Het idee van opdracht 3.7 was om de fout op de waarden te vinden, dit zou gedaan kunnen worden door de meting een aantal keer te doen, 
 # daar het gemiddelde van te nemen en de fout als de standaarddeviatie / sqrt(n) te nemen.
+# aanpassing
